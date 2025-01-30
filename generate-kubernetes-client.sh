026ff5872feb44ec98e7acb58e964df2240a79b8
Michael Gill <michael@northflank.com>
2025-01-30 13:30:35 +0000

    fix: create parent dirs

--- generate-kubernetes-client.sh
+++ generate-kubernetes-client.sh
@@ -6,7 +6,7 @@ function main() {
   rm -rf .temp
   node ./download-crds.js
   ./generate-openapi-spec.sh
-  mkdir kubernetes-client/src/gen
+  mkdir -p kubernetes-client/src/gen
   # see
   # - https://github.com/kubernetes-client/gen/blob/b461333bb57fa2dc2152f939ed70bac3cef2c1f6/openapi/openapi-generator/generate_client_in_container.sh#L82
   # - https://github.com/kubernetes-client/gen/blob/b461333bb57fa2dc2152f939ed70bac3cef2c1f6/openapi/preprocess_spec.py#L565
