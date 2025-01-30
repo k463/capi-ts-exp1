80c20657d6745d8fd8f1f785ebfbb05d68bb2978
Michael Gill <michael@northflank.com>
2025-01-30 13:33:57 +0000

    fix: execute from client dir

--- generate-kubernetes-client.sh
+++ generate-kubernetes-client.sh
@@ -11,7 +11,8 @@ function main() {
   # - https://github.com/kubernetes-client/gen/blob/b461333bb57fa2dc2152f939ed70bac3cef2c1f6/openapi/openapi-generator/generate_client_in_container.sh#L82
   # - https://github.com/kubernetes-client/gen/blob/b461333bb57fa2dc2152f939ed70bac3cef2c1f6/openapi/preprocess_spec.py#L565
   yq e --output-format=json .temp/openapi/openapi.yaml > kubernetes-client/src/gen/swagger.json.unprocessed
-  ./kubernetes-client/generate-client.sh
+  cd ./kubernetes-client/
+  ./generate-client.sh
 }
 
 main
